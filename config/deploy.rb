059ab8f542c723c073a98f1267237811550956d8
Agustin Feuerhake <agustin@platan.us>
2013-12-13 23:51:25 -0300

    changes repository address

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'bank_checker'
+set :repo_url, 'git@github.com:agustinf/bice_check.git'
 
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
